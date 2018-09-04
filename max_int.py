a906cbe67a9a744f404b441d9563bf5dca0e6bfd
Johann Hilmir <brotherjoi@gmail.com>
2018-09-04 10:41:46 +0000

    A more refined solution, all test cases passed on Mimir.

--- max_int.py
+++ max_int.py
@@ -12,17 +12,19 @@
 #   2.1. if the number is negative
 #       print the highest number from the user
 #   2.2. if the number is positive
+#       if the largest number is 0
+#           store the users number as the largest
 #       compare it to the previous number
-#       if it is larger than the previous number
-#           store it as the largest number
-#       if it is smaller than the previous number
-#           go back to step 1
+#           if it is larger than the previous number
+#               store it as the largest number
+#           if it is smaller than the previous number
+#                go back to step 1
 
 max_int = 0
 
 num_int = int(input("Input a number: "))    # Do not change this line
 
-while num_int > 0:
+while num_int >= 0:
     num_int = int(input("Input a number: "))
     if max_int == 0:
         max_int = num_int
@@ -31,5 +33,4 @@ while num_int > 0:
             max_int = num_int
 
 
-# Fill in the missing code
 print("The maximum is", max_int)    # Do not change this line
